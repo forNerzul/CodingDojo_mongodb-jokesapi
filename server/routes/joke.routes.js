8c1e9d60a3d9e881343d93411be311bdfc24e9a6
forNerzul <sbeardman92@gmail.com>
2023-01-31 19:31:29 -0300

    Finalizando "MongoDB Jokes Api"

---
+++ server/routes/joke.routes.js
@@ -0,0 +1,9 @@
+const JokeController = require("../controllers/joke.controller");
+
+module.exports = (app) => {
+    app.get("/jokes", JokeController.findAllJokes);
+    app.get("/jokes/:id", JokeController.findOneSingleJoke);
+    app.put("/jokes/update/:id", JokeController.updateExistingJoke);
+    app.post("/jokes/new", JokeController.createNewJoke);
+    app.delete("/jokes/delete/:id", JokeController.deleteAnExistingJoke);
+};
